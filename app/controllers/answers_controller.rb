f0b116c26efd49ae7bfe5c0eb3cef4471230c5b1
Raghu Betina <raghu@firstdraft.com>
2018-03-07 02:11:05 +0000

    Generate answers resource with starter:resource

---
+++ app/controllers/answers_controller.rb
@@ -0,0 +1,81 @@
+class AnswersController < ApplicationController
+  def index
+    @answers = Answer.all
+
+    render("answers/index.html.erb")
+  end
+
+  def show
+    @answer = Answer.find(params[:id])
+
+    render("answers/show.html.erb")
+  end
+
+  def new
+    @answer = Answer.new
+
+    render("answers/new.html.erb")
+  end
+
+  def create
+    @answer = Answer.new
+
+    @answer.response = params[:response]
+    @answer.question_id = params[:question_id]
+
+    save_status = @answer.save
+
+    if save_status == true
+      referer = URI(request.referer).path
+
+      case referer
+      when "/answers/new", "/create_answer"
+        redirect_to("/answers")
+      else
+        redirect_back(:fallback_location => "/", :notice => "Answer created successfully.")
+      end
+    else
+      render("answers/new.html.erb")
+    end
+  end
+
+  def edit
+    @answer = Answer.find(params[:id])
+
+    render("answers/edit.html.erb")
+  end
+
+  def update
+    @answer = Answer.find(params[:id])
+
+    @answer.response = params[:response]
+    @answer.question_id = params[:question_id]
+
+    save_status = @answer.save
+
+    if save_status == true
+      referer = URI(request.referer).path
+
+      case referer
+      when "/answers/#{@answer.id}/edit", "/update_answer"
+        redirect_to("/answers/#{@answer.id}", :notice => "Answer updated successfully.")
+      else
+        redirect_back(:fallback_location => "/", :notice => "Answer updated successfully.")
+      end
+    else
+      render("answers/edit.html.erb")
+    end
+  end
+
+  def destroy
+    @answer = Answer.find(params[:id])
+
+    @answer.destroy
+
+    if URI(request.referer).path == "/answers/#{@answer.id}"
+      redirect_to("/", :notice => "Answer deleted.")
+    else
+      redirect_back(:fallback_location => "/", :notice => "Answer deleted.")
+    end
+  end
+end
