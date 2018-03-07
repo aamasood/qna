7be3b47a17339c3e0cabf5e87f06baf45ead858f
Raghu Betina <raghu@firstdraft.com>
2018-03-07 02:12:05 +0000

    Whitelist columns in Question for ActiveAdmin

--- app/admin/questions.rb
+++ app/admin/questions.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Question do
+
+ permit_params :inquiry, :subject_id, :high_level_question
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
